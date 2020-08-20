d4a4c166931d7a05d74fb83899bd9a2e6fdbbbb7
Raphael Luckom <raphaelluckom@gmail.com>
2020-08-20 15:18:35 -0400

    add subject alt name in cert, dns

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,5 +1,6 @@
 resource "aws_acm_certificate" "raphaelluckom_cert" {
-  domain_name       = "www.raphaelluckom.com"
+  domain_name       = "raphaelluckom.com"
+  subject_alternative_names = ["www.raphaelluckom.com"]
   validation_method = "DNS"
 
   lifecycle {
@@ -9,6 +10,6 @@ resource "aws_acm_certificate" "raphaelluckom_cert" {
 
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn = "${aws_acm_certificate.raphaelluckom_cert.arn}"
-  validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
+  validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}", "${aws_route53_record.cert_validation_www.fqdn}"]
 }
 
--- terraform/route53.tf
+++ terraform/route53.tf
@@ -3,11 +3,19 @@ data "aws_route53_zone" "selected" {
   private_zone = false
 }
 
-resource "aws_route53_record" "cert_validation" {
+resource "aws_route53_record" "cert_validation_www" {
   name = "${aws_acm_certificate.raphaelluckom_cert.domain_validation_options.0.resource_record_name}"
   type = "${aws_acm_certificate.raphaelluckom_cert.domain_validation_options.0.resource_record_type}"
   zone_id = "${data.aws_route53_zone.selected.id}"
   records = ["${aws_acm_certificate.raphaelluckom_cert.domain_validation_options.0.resource_record_value}"]
+  ttl = 60
+}
+
+resource "aws_route53_record" "cert_validation" {
+  name = "${aws_acm_certificate.raphaelluckom_cert.domain_validation_options.1.resource_record_name}"
+  type = "${aws_acm_certificate.raphaelluckom_cert.domain_validation_options.1.resource_record_type}"
+  zone_id = "${data.aws_route53_zone.selected.id}"
+  records = ["${aws_acm_certificate.raphaelluckom_cert.domain_validation_options.1.resource_record_value}"]
   ttl = 60
 }
 
@@ -17,4 +25,16 @@ resource "aws_route53_record" "www_raphaelluckom" {
   type    = "CNAME"
   ttl     = "300"
   records = ["${aws_cloudfront_distribution.raphaelluckom_distribution.domain_name}"]
+}
+
+resource "aws_route53_record" "raphaelluckom" {
+  zone_id = "${data.aws_route53_zone.selected.id}"
+  name    = "raphaelluckom.com"
+  type    = "A"
+
+  alias {
+    zone_id = "${aws_cloudfront_distribution.raphaelluckom_distribution.hosted_zone_id}"
+    name                   = "${aws_cloudfront_distribution.raphaelluckom_distribution.domain_name}"
+    evaluate_target_health = true
+  }
 }
