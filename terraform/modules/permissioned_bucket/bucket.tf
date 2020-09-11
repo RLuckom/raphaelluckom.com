834d520e07b919a36d8a133657958b265be86fc9
Raphael Luckom <raphaelluckom@gmail.com>
2020-09-11 16:31:47 -0400

    add cors, website to s3 module

--- terraform/modules/permissioned_bucket/bucket.tf
+++ terraform/modules/permissioned_bucket/bucket.tf
@@ -1,3 +1,21 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket
+  dynamic "website" {
+    for_each = var.website_configs
+    content {
+      index_document = website.value.index_document
+      error_document = website.value.error_document
+    }
+  }
+
+  dynamic "cors_rule" {
+    for_each = var.cors_rules
+    content {
+      allowed_headers = cors_rule.value.allowed_headers
+      allowed_origins = cors_rule.value.allowed_origins
+      allowed_methods = cors_rule.value.allowed_methods
+      expose_headers = cors_rule.value.expose_headers
+      max_age_seconds = cors_rule.value.max_age_seconds
+    }
+  }
 }
--- terraform/modules/permissioned_bucket/variables.tf
+++ terraform/modules/permissioned_bucket/variables.tf
@@ -1,3 +1,27 @@
 variable "bucket" {
   type = string
 }
+
+variable "website_configs" {
+  type = list(object({
+    index_document = string
+    error_document = string
+  }))
+  default = []
+}
+
+variable "act" {
+  type = string
+  default = "private"
+}
+
+variable cors_rules {
+  type = list(object({
+    allowed_headers = list(string)
+    allowed_methods = list(string)
+    allowed_origins = list(string)
+    expose_headers = list(string)
+    max_age_seconds = number
+  }))
+  default = []
+}
