c9ebb46b5ddf26d14df7d1e18cfd45cdb74f198f
Raphael Luckom <raphaelluckom@gmail.com>
2020-11-16 12:42:21 -0500

    md template specification, assets

--- terraform/functions/templates/site-sync/config.js
+++ terraform/functions/templates/site-sync/config.js
@@ -14,84 +14,14 @@ const mdr = require('markdown-it')({
     return ''; // use external default escaping
   }
 }).use(require('markdown-it-footnote'))
-const t = `<!DOCTYPE html>
-<html lang="en-us">
-  <head>
-    <title>
-         <%= title %>
-    </title>
-  </head>
-  <body>
 
-<section id=content>
-  <h1> <%= title %> </h1>
-
-  
-    <div id=sub-header>
-      <%= date %>
-    </div>
-    <div class="entry-content">
-<%= content %>
-    </div>
-  </body>
-</html>
-`
-
-const namespaceDetails = {
-  name: 'S3',
-  constructorArgs: {}
-};
-
-const putObject = {
-  dataSource: 'AWS',
-  namespaceDetails,
-  name: 'putObject',
-  value: {
-    path: _.identity,
-  },
-  apiMethod: 'putObject',
-  requiredParams: {
-    Bucket: {},
-    Body: {},
-    Key: {},
-  },
-  optionalParams: {
-    CacheControl: {},
-    ContentDisposition: {},
-    ContentEncoding: {},
-    ContentLanguage: {},
-    ContentLength: {},
-    ContentMD5: {},
-    ContentType: {},
-    ExpectedBucketOwner: {},
-    Expires: {},
-    GrantFullControl: {},
-    GrantRead: {},
-    GrantReadACP: {},
-    GrantWriteACP: {},
-    Metadata: {},
-    ObjectLockLegalHoldStatus: {},
-    ObjectLockMode: {},
-    ObjectLockRetainUntilDate: {},
-    RequestPayer: {},
-    SSECustomerAlgorithm: {},
-    SSECustomerKey: {}, 
-    SSECustomerKeyMD5: {},
-    SSEKMSEncryptionContext: {},
-    SSEKMSKeyId: {},
-    ServerSideEncryption: {},
-    StorageClass: {},
-    Tagging: {},
-    WebsiteRedirectLocation: {}
-  }
-};
-
+//TODO: make this also accept toml, json front matter
 function parsePost(s) {
   const t = _(s.split('\n')).filter().map(_.trim).value()
   if (t[0] === '---') {
     let started = false
     let frontMatter = ''
-    let post = ''
+    let content = ''
     for (r of t.slice(1)) {
       if (r === '---') {
         if (!started) {
@@ -99,7 +29,7 @@ function parsePost(s) {
         }
       } else {
         if (started) {
-          post += r + "\n"
+          content += r + "\n"
         } else {
           frontMatter += r + '\n'
         }
@@ -110,7 +40,7 @@ function parsePost(s) {
       if (fm.date) {
         fm.date = moment(fm.date)
       }
-      return { frontMatter: fm, post: post, raw:s }
+      return { frontMatter: fm, content, raw:s }
     } catch(e) {
       console.log(e)
       return { raw: s} 
@@ -120,10 +50,9 @@ function parsePost(s) {
   }
 }
 
-const temp = _.template(t)
 module.exports = {
   stages: {
-    getChangedMarkdown: {
+    getChangedItem: {
       index: 0,
       dependencies: {
         text: {
@@ -140,36 +69,87 @@ module.exports = {
         }
       },
     },
-    postMarkdownToWebsiteBucket: {
+    parseItemStructure: {
       index: 1,
+      transformers: {
+        structuredItem: {
+          helper: 'transform',
+          params: {
+            func: {
+              value: (x) =>  parsePost(x.toString())
+            },
+            arg: {ref: 'getChangedItem.results.text[0].Body' }
+          }
+        }
+      },
+      dependencies: {
+        template: {
+          action: 'exploranda',
+          params: {
+            accessSchema: {
+              helper: 'transform',
+              params: {
+                arg: { ref: 'stage.structuredItem.frontMatter' },
+                func: {
+                  value: ({baseUrl, template}) => {
+                    let url
+                    if (_.isString(template)) {
+                      try {
+                        url = new URL(template)
+                      } catch(e) {
+                        url = new URL(template, baseUrl)
+                      }
+                    } else {
+                      url = new URL(template.path, template.baseUrl)
+                    }
+                    const as = {
+                      dataSource: 'GENERIC_API',
+                      host: url.host,
+                      path: url.pathname,
+                      protocol: _.trimEnd(url.protocol, ':') + '://',
+                    }
+                    console.log(as)
+                    return as
+                  }
+                }
+              }
+            },
+            params: {},
+          },
+        },
+      },
+    },
+    postItemToWebsiteBucket: {
+      index: 2,
       transformers: {
         fileContent: {
           helper: 'transform',
           params: {
-            func: {
-              value: (x) => {
-                const post = parsePost(x.toString())
-                return temp({
-                  title: post.frontMatter.title,
-                  date: post.frontMatter.date,
-                  content: mdr.render(post.post, {})
-                })
-              }
+            arg: {
+              all: {
+                template: {ref: 'parseItemStructure.results.template[0].body' },
+                doc: { ref: 'parseItemStructure.vars.structuredItem' },
+              },
             },
-            arg: {ref: 'getChangedMarkdown.results.text[0].Body' }
-          }
-        }
+            func: { 
+              value: ({template, doc}) => {
+                return _.template(template.toString())({...doc.frontMatter, ...{ content: mdr.render(doc.content, {})}})
+              }
+            }
+          },
+        },
       },
       dependencies: {
         uploadHtml: {
           action: 'exploranda',
           params: {
-            accessSchema: {value: putObject},
+            accessSchema: {value: 'dataSources.AWS.s3.putObject'},
             params: {
               explorandaParams: {
                 Body: {ref: 'stage.fileContent' },
                 Bucket: '${website_bucket}',
-                ContentType: 'text/html',
+                ContentType: 'text/html; charset=utf-8',
+                CacheControl: 'no-cache',
                 Key: {
                   helper: 'transform', 
                   params:{
@@ -191,6 +171,4 @@ module.exports = {
       },
     }
   },
-  cleanup: {
-  }
 }
---
+++ terraform/sites/test.raphaelluckom.com/update.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+aws s3 sync . s3://test.raphaelluckom.com
