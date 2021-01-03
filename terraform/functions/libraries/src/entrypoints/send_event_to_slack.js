cd32dab0d9cb9b7cf4c79c402429782807917d5c
Raphael Luckom <raphaelluckom@gmail.com>
2021-01-03 15:34:14 -0500

    clarify error reporting

--- terraform/functions/libraries/src/entrypoints/send_event_to_slack.js
+++ terraform/functions/libraries/src/entrypoints/send_event_to_slack.js
@@ -5,16 +5,30 @@ const needle = require('needle')
 const slackCredentialsParameter = process.env.SLACK_CREDENTIAL_PARAM
 const slackChannel = process.env.SLACK_CHANNEL
 
+function constructReadableError(evt) {
+  const err = _.isString(_.get(evt, 'responsePayload')) ? JSON.parse(_.get(evt, 'responsePayload')) : _.get(evt, 'responsePayload')
+  return `Function: ${_.get(evt, 'requestContext.functionArn')}\nResult: ${_.get(evt, 'requestContext.condition')}\nRequestId: ${_.get(evt, 'requestContext.requestId')}\nErrorType: ${_.get(err, 'errorType')}\nError Message: ${_.get(err, 'errorMessage')}\nStack trace --\n${_.get(err, 'trace[0]')}`
+}
+
 function main(event, context, callback) {
   new AWS.SSM().getParameter({
     Name: slackCredentialsParameter,
     WithDecryption: true,
   }, (e, r) => {
+    let msg = JSON.stringify(event)
     const {token} = JSON.parse(r.Parameter.Value)
+    const status = _.get(event, 'requestContext.condition')
+    if (status !== 'Success') {
+      try {
+        msg = constructReadableError(event)
+      } catch(e) {
+        console.log(e)
+      }
+    }
     var options = {
       headers: { 'Authorization': `Bearer ${token}` }
     }
-    needle.request('POST', 'https://slack.com/api/chat.postMessage', { channel: slackChannel, text: JSON.stringify(event) }, options, (e, r) => {
+    needle.request('POST', 'https://slack.com/api/chat.postMessage', { channel: slackChannel, text: msg }, options, (e, r) => {
       console.log(e)
     })
   })
