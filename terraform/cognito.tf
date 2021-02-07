56325dbeb6ffd9e4228cde140db0b95653c000f9
Raphael Luckom <raphaelluckom@gmail.com>
2021-02-07 14:15:17 -0500

    cognito user pool with own domain

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -13,6 +13,7 @@ locals {
     "aws.cognito.signin.user.admin","openid"
   ]
   allowed_oauth_flows_user_pool_client = true
+  zone = "raphaelluckom.com"
 }
 
 resource aws_cognito_user_pool user_pool {
@@ -52,8 +53,48 @@ resource aws_cognito_user_pool_client client {
   allowed_oauth_flows_user_pool_client = local.allowed_oauth_flows_user_pool_client
 }
 
-resource "aws_cognito_user_pool_domain" "domain" {
-  domain          = "test-raphaelluckom-com"
+resource aws_cognito_user_pool_domain domain {
+  domain          = "auth.test.raphaelluckom.com"
+  certificate_arn = aws_acm_certificate.cert.arn
   user_pool_id    = aws_cognito_user_pool.user_pool.id
 }
 
+data aws_route53_zone selected {
+  name         = local.zone
+  private_zone = false
+}
+
+resource aws_route53_record cert_validation {
+  name            = aws_acm_certificate.cert.domain_validation_options.*.resource_record_name[0]
+  records         = aws_acm_certificate.cert.domain_validation_options.*.resource_record_value
+  type            = aws_acm_certificate.cert.domain_validation_options.*.resource_record_type[0]
+  zone_id         = data.aws_route53_zone.selected.zone_id
+  ttl             = 60
+}
+
+resource aws_route53_record auth_a_record {
+  name    = "auth.test.${local.zone}"
+  type    = "A"
+  zone_id = data.aws_route53_zone.selected.id
+  alias {
+    evaluate_target_health = false
+    name                   = aws_cognito_user_pool_domain.domain.cloudfront_distribution_arn
+    # This zone_id is fixed
+    zone_id = "Z2FDTNDATAQYW2"
+  }
+}
+
+resource aws_acm_certificate cert {
+  domain_name       = "auth.test.${var.domain_name}"
+  subject_alternative_names = []
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource aws_acm_certificate_validation cert_validation {
+  certificate_arn = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+}
