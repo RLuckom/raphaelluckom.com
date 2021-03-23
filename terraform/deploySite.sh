602f574b6d8654b39c27e36a350b0a5ca3a589d4
Raphael Luckom <raphaelluckom@gmail.com>
2021-03-23 14:54:03 -0400

    prosemirror basic poc

---
+++ terraform/deploySite.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ./frontend/lith
+npm run browserify
+cd -
+aws s3 cp sites/private/index.html s3://test.raphaelluckom.com && aws s3 cp sites/private/assets/styles/styles.css s3://test.raphaelluckom.com/assets/styles/ && aws s3 cp frontend/lith/index-dist.js s3://test.raphaelluckom.com/assets/js
