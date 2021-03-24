9088027dbe0b2ebad8bfffdbb3f705898b819ab2
Raphael Luckom <raphaelluckom@gmail.com>
2021-03-24 11:09:41 -0400

    update deploysite

--- terraform/deploySite.sh
+++ terraform/deploySite.sh
@@ -2,4 +2,5 @@
 cd ./frontend/lith
 npm run browserify
 cd -
+cp frontend/lith/index-dist.js sites/private/assets/js
 aws s3 cp sites/private/index.html s3://test.raphaelluckom.com && aws s3 cp sites/private/assets/styles/styles.css s3://test.raphaelluckom.com/assets/styles/ && aws s3 cp frontend/lith/index-dist.js s3://test.raphaelluckom.com/assets/js/
