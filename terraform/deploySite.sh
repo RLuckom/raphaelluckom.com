a38231a5766c43540ca79e839ff6e83fdfef9d8d
Raphael Luckom <raphaelluckom@gmail.com>
2021-03-24 09:55:38 -0400

    some post ui dependencies

--- terraform/deploySite.sh
+++ terraform/deploySite.sh
@@ -2,4 +2,4 @@
 cd ./frontend/lith
 npm run browserify
 cd -
-aws s3 cp sites/private/index.html s3://test.raphaelluckom.com && aws s3 cp sites/private/assets/styles/styles.css s3://test.raphaelluckom.com/assets/styles/ && aws s3 cp frontend/lith/index-dist.js s3://test.raphaelluckom.com/assets/js
+aws s3 cp sites/private/index.html s3://test.raphaelluckom.com && aws s3 cp sites/private/assets/styles/styles.css s3://test.raphaelluckom.com/assets/styles/ && aws s3 cp frontend/lith/index-dist.js s3://test.raphaelluckom.com/assets/js/
--- terraform/frontend/lith/index.js
+++ terraform/frontend/lith/index.js
@@ -1,20 +1,47 @@
 const {EditorState} = require("prosemirror-state")
 const {EditorView} = require("prosemirror-view")
-const {Schema, DOMParser} = require("prosemirror-model")
-const {schema} = require("prosemirror-schema-basic")
-const {addListNodes} = require("prosemirror-schema-list")
 const {exampleSetup} = require("prosemirror-example-setup")
 
-// Mix the nodes from prosemirror-schema-list into the basic schema to
-// create a schema with list support.
-const mySchema = new Schema({
-  nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
-  marks: schema.spec.marks
-})
+const {
+  defaultMarkdownParser,
+  defaultMarkdownSerializer,
+  schema,
+} = require('prosemirror-markdown')
 
-window.view = new EditorView(document.querySelector("#editor"), {
-  state: EditorState.create({
-    doc: DOMParser.fromSchema(mySchema).parse(document.querySelector("#content")),
-    plugins: exampleSetup({schema: mySchema})
-  })
-})
+// https://gist.github.com/mbrehin/05c0d41a7e50eef7f95711e237502c85
+// script to replace <textarea> elements in forms with prosemirror editors 
+// ( if they have the .prosemirror class ) 
+function initWysiwyEditors() {
+  // Loop over every textareas to replace with dynamic editor
+  for (const area of document.querySelectorAll('textarea.prosemirror')) {
+    // Hide textarea
+    area.style.display = 'none'
+    // Create container zone
+    const container = document.createElement('div')
+    container.classList = area.classList
+    if (area.nextSibling) {
+      area.parentElement.insertBefore(container, area.nextSibling)
+    } else {
+      area.parentElement.appendChild(container)
+    }
+
+    // Load editor view
+    const view = new EditorView(container, {
+      // Set initial state
+      state: EditorState.create({
+        doc: defaultMarkdownParser.parse(area.value),
+        plugins: exampleSetup({ schema }),
+      }),
+      dispatchTransaction(tr) {
+        const { state } = view.state.applyTransaction(tr)
+        view.updateState(state)
+        // Update textarea only if content has changed
+        if (tr.docChanged) {
+          area.value = defaultMarkdownSerializer.serialize(tr.doc)
+        }
+      },
+    })
+  }
+}
+
+document.addEventListener('DOMContentLoaded', initWysiwyEditors)
