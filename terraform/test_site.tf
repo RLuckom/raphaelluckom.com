7b21c043d57752e3a7ef4a26d32b744a90bbf700
Raphael Luckom <raphaelluckom@gmail.com>
2021-03-24 11:40:00 -0400

    remove unnecessary policy

--- terraform/test_site.tf
+++ terraform/test_site.tf
@@ -37,14 +37,6 @@ module cognito_identity_management {
   system_id = local.variables.cognito_system_id
   client_id               = module.cognito_user_management.user_pool_client.id
   provider_endpoint           = module.cognito_user_management.user_pool.endpoint
-  authenticated_policy_statements = [{
-    actions = [
-      "s3:ListAllMyBuckets",
-    ]
-    resources = [
-      "arn:aws:s3:::*",
-    ]
-  }]
 }
 
 resource random_password nonce_signing_secret {
@@ -80,6 +72,7 @@ module get_access_creds {
   user_pool_endpoint = module.cognito_user_management.user_pool.endpoint
   api_path = "/api/actions/access/credentials"
   gateway_name_stem = "test_site"
+  client_id = module.cognito_user_management.user_pool_client.id
   aws_sdk_layer = module.aws_sdk.layer_config
 }
 
@@ -90,27 +83,13 @@ module test_site {
     subsystem_name = "test"
   }
   asset_path = "${path.root}/sites/private"
-  lambda_authorizers = {
-    "default" = {
-    name = "default"
-    audience = [module.cognito_user_management.user_pool_client.id]
-    issuer = "https://${module.cognito_user_management.user_pool.endpoint}"
-    identity_sources = ["$request.header.Authorization"]
-    }
-  }
+  lambda_authorizers = module.get_access_creds.lambda_authorizer_config
   lambda_origins = module.get_access_creds.lambda_origins
   routing = {
     domain_parts = module.visibility_system.serverless_site_configs["test"].domain_parts
     route53_zone_name = var.route53_zone_name
   }
-  access_control_function_qualified_arns = [{
-    refresh_auth   = module.access_control_functions.refresh_auth.lambda.qualified_arn
-    parse_auth   = module.access_control_functions.parse_auth.lambda.qualified_arn
-    check_auth   = module.access_control_functions.check_auth.lambda.qualified_arn
-    sign_out   = module.access_control_functions.sign_out.lambda.qualified_arn
-    http_headers   = module.access_control_functions.http_headers.lambda.qualified_arn
-    move_cookie_to_auth_header   = module.access_control_functions.move_cookie_to_auth_header.lambda.qualified_arn
-  }]
+  access_control_function_qualified_arns = [module.access_control_functions.access_control_function_qualified_arns]
   site_bucket = local.variables.site_bucket
   coordinator_data = module.visibility_system.serverless_site_configs["test"]
   subject_alternative_names = ["www.test.raphaelluckom.com"]
