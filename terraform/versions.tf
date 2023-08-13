ee1c684f036c17562e0fa569ede821593cc4b826
Raphael Luckom <raphaelluckom@gmail.com>
2023-08-13 17:33:23 -0400

    bump provider versions

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -28,7 +28,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.22"
+      version = "~> 5.12.0"
     }
   }
   required_version = ">= 1.2.4"
